aba39e972a157f22bc73afc240af8279cfee7100
jeessy2 <6205259+jeessy2@users.noreply.github.com>
2024-02-02 21:34:56 +0800

    feat: use regexp to find ip

--- ban/check_ip.go
+++ ban/check_ip.go
@@ -5,8 +5,15 @@ import (
 	"log"
 	"net"
 	"os"
+	"regexp"
 	"strings"
 )
+
+// Ipv4Reg IPv4正则
+var ipv4Reg = regexp.MustCompile(`((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])`)
+
+// Ipv6Reg IPv6正则
+var ipv6Reg = regexp.MustCompile(`((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))`)
 
 // checkIP 检查IP地址是否合法
 func checkIP(ips []string) (toBeBanIPs []string, err error) {
@@ -23,17 +30,22 @@ func checkIP(ips []string) (toBeBanIPs []string, err error) {
 			log.Println(err)
 			continue
 		}
-		ip := net.ParseIP(ips[i])
+		// 使用正则查找IPv4/IPv6
+		result := ipv4Reg.FindString(ips[i])
+		if result == "" {
+			result = ipv6Reg.FindString(ips[i])
+		}
+		ip := net.ParseIP(result)
 		if ip == nil {
 			err = fmt.Errorf(errStr+"IP地址: %s 非法, 将不会被Ban! ", ips[i])
 			continue
 		}
 		if ip.IsPrivate() {
-			err = fmt.Errorf(errStr+"IP地址: %s 不能为私网地址, 将不会被Ban! ", ips[i])
+			err = fmt.Errorf(errStr+"IP地址: %s 不能为私网地址, 将不会被Ban! ", ip.String())
 			continue
 		}
 		if ip.IsLoopback() {
-			err = fmt.Errorf(errStr+"IP地址: %s 不能为Loopback, 将不会被Ban! ", ips[i])
+			err = fmt.Errorf(errStr+"IP地址: %s 不能为Loopback, 将不会被Ban! ", ip.String())
 			continue
 		}
 		// 如果在白名单中, 将不会被Ban
@@ -41,7 +53,7 @@ func checkIP(ips []string) (toBeBanIPs []string, err error) {
 		for j := 0; j < len(whiteList); j++ {
 			if whiteList[j].Contains(ip) {
 				find = true
-				err = fmt.Errorf(errStr+"IP地址: %s 在白名单中, 将不会被Ban! ", ips[i])
+				err = fmt.Errorf(errStr+"IP地址: %s 在白名单中, 将不会被Ban! ", ip.String())
 				break
 			}
 		}
